7f467370a12c1d6359ada5f6bc9de12e7c31dd32
mindball <mincho.balaliew@gmail.com>
2020-10-19 09:35:52 +0300

    Create basic HTTP Server over TCP

---
+++ WorkShopSus/SUS/Apps/MyFirstMvcApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer();
+
+            server.Start(80);
+
+            server.AddRoute("/", (request) =>
+            {
+                return new HttpResponseHeader();
+            });
+
+
+
+            server.AddRoute("/", HomePage);
+
+            server.AddRoute("/about", About);
+
+            server.AddRoute("/users/login", Login);
+
+        }
+
+        static HttpResponseHeader HomePage(HttpRequestHeader request)
+        {
+
+        }
+
+        static HttpResponseHeader About(HttpRequestHeader request)
+        {
+
+        }
+
+        static HttpResponseHeader Login(HttpRequestHeader request)
+        {
+
+        }
+    }
+}
---
+++ WorkShopSus/SUS/SUS.HTTP/HttpServer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUS.HTTP
+{
+    public class HttpServer : IHttpServer
+    {
+        IDictionary<string, Func<HttpRequest, HttpResponse>>
+            routeTable = new Dictionary<string, Func<HttpRequest, HttpResponse>>();
+
+
+        public void AddRoute(string path, Func<HttpRequest, HttpResponse> action)
+        {
+            if (routeTable.ContainsKey(path))
+            {
+                routeTable[path] = action;
+            }
+            else
+            {
+                routeTable.Add(path, action);
+            }
+        }
+
+        public async Task StartAsync(int port)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, port);
+            tcpListener.Start();
+
+            while(true)
+            {
+                var tcpClient = await tcpListener.AcceptTcpClientAsync();
+                ProcessClientAsync(tcpClient);
+            }
+        }
+
+        private async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            try
+            {
+                using NetworkStream stream = tcpClient.GetStream();
+
+                var data = new List<byte>();
+                int position = 0;
+
+                byte[] buffer = new byte[HttpConstants.BufferSize]; //chunk
+
+                while(true)
+                {
+                    int count =
+                            await stream.ReadAsync(buffer, position, buffer.Length);
+                    position += count;
+
+                    //
+                    if(count < buffer.Length)
+                    {
+                        var partialBuffer = new byte[count];
+                        Array.Copy(buffer, partialBuffer, count);
+
+                        data.AddRange(partialBuffer);
+                        break;
+                    }
+                    else
+                    {
+                        data.AddRange(buffer);
+                    }
+                }
+
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
