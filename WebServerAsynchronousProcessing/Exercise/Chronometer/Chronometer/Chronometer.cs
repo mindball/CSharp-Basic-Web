ba8a1efea6ce6263e2514236cd10dd41a637d747
mindball <mincho.balaliew@gmail.com>
2020-10-20 13:38:38 +0300

    Create sync Chronometer

---
+++ WebServerAsynchronousProcessing/Exercise/Chronometer/Chronometer/Chronometer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private List<string> laps;
+        private Stopwatch watch;
+        private string totalElapsedTime;
+
+        public Chronometer()
+        {
+            this.laps = new List<string>();
+            this.watch = new Stopwatch();
+        }
+
+        public string GetTime => this.totalElapsedTime;
+
+        public IList<string> Laps => this.laps.AsReadOnly();
+
+        public string Lap()
+        {
+            var timeElapse = this.watch.Elapsed.ToString();
+            this.laps.Add(timeElapse);
+
+            return timeElapse;
+        }
+
+        public void Reset()
+        {
+            this.watch.Reset();
+            this.totalElapsedTime = this.watch.Elapsed.ToString();
+            this.laps.Clear();            
+        }
+
+        public void Start()
+        {                     
+            this.watch.Start();
+        }
+
+        public void Stop()
+        {
+            this.totalElapsedTime = this.watch.Elapsed.ToString();
+            this.watch.Stop();
+        }
+    }
+}
---
+++ WebServerAsynchronousProcessing/Exercise/Chronometer/Chronometer/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Chronometer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (string.Equals("start", input))
+            {
+                IChronometer chr = new Chronometer();
+                chr.Start();
+
+                while (true)
+                {
+                    input = Console.ReadLine();
+                    if (string.Equals("exit", input))
+                    {
+                        chr.Stop();
+                        break;
+                    }
+
+                    switch (input)
+                    {
+                        case "start": chr.Start();
+                            break;
+
+                        case "lap":
+                            Console.WriteLine(chr.Lap());
+                            break;
+                        case "laps":
+                            Console.WriteLine("Laps:");
+                            foreach (var lap in chr.Laps)
+                            {
+                                Console.WriteLine(lap);
+                            }
+                            break;
+                        case "stop": chr.Stop();
+                            break;
+                        case "time":
+                            Console.WriteLine(chr.GetTime);
+                            break;
+                        case "reset": chr.Reset();
+                            break;                       
+                    }
+
+                    ;
+                }
+            }
+
+
+        }
+    }
+}
