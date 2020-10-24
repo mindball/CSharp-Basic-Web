87378af9628f08a494717f9c226ba4bfe2ca6bc8
mindball <mincho.balaliew@gmail.com>
2020-10-24 09:09:20 +0300

    test

---
+++ WorkShopSus/SUS/Demo.TestRequest/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using SIS.HTTP.DiffApproach.Enums;
+using SIS.HTTP.DiffApproach.Requests.Contracts;
+using SIS.HTTP.DiffApproach.Responses.Contracts;
+using SIS.WebServer.DiffApproach.Result;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.TestRequest
+{
+    public class HomeController
+    {
+        public IHttpResponse Index(IHttpRequest request)
+        {
+            string content = "<h1>Hello world!</h1>";
+
+            return new HtmlResult(content, HttpResponseStatusCode.Ok);
+        }
+    }
+}
---
+++ WorkShopSus/SUS/Demo.TestRequest/Program.cs
@@ -0,0 +1,30 @@
+﻿using SIS.HTTP.DiffApproach.Enums;
+using SIS.HTTP.DiffApproach.Headers;
+using SIS.HTTP.DiffApproach.Requests;
+using SIS.HTTP.DiffApproach.Responses;
+using SIS.HTTP.DiffApproach.Responses.Contracts;
+using SIS.WebServer.DiffApproach;
+using SIS.WebServer.DiffApproach.Routing;
+using System;
+using System.Globalization;
+using System.Text;
+
+namespace Demo.TestRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Add(HttpRequestMethod.Get,
+                "/",
+                request => new HomeController().Index(request));
+
+            Server server = new Server(8000, serverRoutingTable);
+
+            server.Run();
+
+        }
+    }
+}
